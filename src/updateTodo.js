b7d7555ebab2f80d4ee5bea10d45d0b3d9dc451c
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-03-14 23:56:46 +0100

    feat(update-post): implement updatePost lambda function

--- src/updateTodo.js
+++ src/updateTodo.js
@@ -1,50 +1,38 @@
 'use strict'
 
-const AWS = require('aws-sdk')
 const uuid = require('uuid')
 
-const dynamodb = AWS.DynamoDB.DocumentClient()
+const {
+  marshall,
+  unmarshall,
+  client: db,
+  PutItemCommand,
+} = require("../init-db")
 
-module.exports.updatePost = function(event, context, callback) {
-  const datetime = new Date().toISOString()
-  const data = JSON.parse(event.body)
-
-  if (typeof data.task !== 'string' || typeof data.done !== 'boolean') {
-    console.error('Invalid entry for field task or done')
-    const response = {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Invald entry for field task or done" })
-    }
-
-    callback(new Error(response))
-    return
-  }
-
+module.exports.updatePost = function (event, context, callback) {
+  const response = { statusCode: 200 }
   const params = {
-    TableName: 'todos',
-    key: {
-      id: event.pathParameters.id
-    },
-    ExpressionAttributeValues: {
-      ':t': data.task,
-      ':d': data.done,
-      ':u': datetime
-    },
-    UpdateExpression: 'set task = :t, done = :d, updatedAt = :u'
+    TableName: process.env.TABLE_NAME,
+    Item: marshall(JSON.stringify(event.body) || {})
   }
 
-  dynamodb.update(params, function (error, data) {
+  db.send(new PutItemCommand(params), function(error, data) {
     if (error) {
       console.error(error)
-      callback(new Error(error))
+      response.statusCode = 500
+      response.body = JSON.stringify({
+        message: error.message || "failed to update posts.",
+        rawData: error.stack
+      })
+
+      callback(new Error(response))
       return
     }
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(data.Item)
-    }
-
-    callback(null, response)
+    response.body = JSON.stringify({
+      message: "successfully updated post.",
+      data: data ? unmarshall(data) : {},
+      rawData: data 
+    })
   })
 }
